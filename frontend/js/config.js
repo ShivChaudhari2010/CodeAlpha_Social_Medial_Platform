098a0137107d45946a10e19b31f6d6cc43d3a492
Shiv <steveskc2010@gmail.com>
2026-07-27 17:08:03 +0530

    add backend url
    add backend url from render

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -3,12 +3,12 @@ console.log("Origin:", window.location.origin);
 
 const host = window.location.hostname;
 
-const API_BASE_URL = "http://localhost:5000/api/v1";
+// const API_BASE_URL = "http://localhost:5000/api/v1";
 
-// const API_BASE_URL =
-//   host === "localhost" || host === "127.0.0.1"
-//     ? "http://localhost:5000/api/v1"
-//     : "https://your-real-backend.onrender.com/api/v1";
+const API_BASE_URL =
+  host === "localhost" || host === "127.0.0.1"
+    ? "http://localhost:5000/api/v1"
+    : "https://codealpha-social-medial-platform.onrender.com";
 
 const TOKEN_KEY = "token";
 const USER_KEY = "user";
