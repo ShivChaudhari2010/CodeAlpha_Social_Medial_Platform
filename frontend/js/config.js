f19007b2b44a9a70efd0b5e79205bd96cd1674f6
Shiv <steveskc2010@gmail.com>
2026-07-27 18:14:03 +0530

    update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,27 +1,9 @@
-// console.log("Hostname:", window.location.hostname);
-// console.log("Origin:", window.location.origin);
-
-// const host = window.location.hostname;
-
-// // const API_BASE_URL = "http://localhost:5000/api/v1";
-
-// const API_BASE_URL = 
-//   host === "localhost"
-//     ? "http://localhost:5000/api/v1"
-//     : "https://codealpha-social-medial-platform.onrender.com";
-
-// const TOKEN_KEY = "token";
-// const USER_KEY = "user";
-
-
-
 
 // Backend API URL
-
 const API_BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:5000/api/v1"
-    : "https://YOUR-BACKEND.onrender.com/api/v1";
+    : "https://codealpha-social-medial-platform.onrender.com/api/v1";
 
 // Local Storage Keys
 const TOKEN_KEY = "token";
