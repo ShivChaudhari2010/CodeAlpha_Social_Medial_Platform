ea83bd988b0af4bdebe0eab60d44406d6772c4c8
Shiv <steveskc2010@gmail.com>
2026-08-08 21:24:09 +0530

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,14 +1,19 @@
 console.log("Hostname:", window.location.hostname);
 console.log("Origin:", window.location.origin);
 
-const host = window.location.hostname;
+// const host = window.location.hostname;
 
-// const API_BASE_URL = "http://localhost:5000/api/v1";
+// const API_BASE_URL =
+//   host === "localhost" || host === "127.0.0.1"
+//     ? "http://localhost:5000/api/v1"
+//     : "https://mini-social-media-backend-ndjk.onrender.com/api/v1";
 
-const API_BASE_URL =
-  host === "localhost" || host === "127.0.0.1"
-    ? "http://localhost:5000/api/v1"
-    : "https://mini-social-media-backend-ndjk.onrender.com/api/v1";
+const CONFIG = {
+  API_BASE_URL:
+    window.location.hostname === "localhost"
+      ? "http://localhost:5000/api/v1"
+      : "https://mini-social-media-backend-ndjk.onrender.com/api/v1"
+};
 
 const TOKEN_KEY = "token";
 const USER_KEY = "user";
